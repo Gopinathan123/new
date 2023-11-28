95740e1c54101cf8045f7c4fa9070025de043c94
LENOVO <LENOVO@DESKTOP-1VN6NSF>
2023-11-28 16:02:59 +0530

    Test

---
+++ src/test/java/bank/openAccount.java
@@ -0,0 +1,12 @@
+package bank;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class openAccount {
+	@Test
+	public void open()
+	{
+		Reporter.log("Account opening is successfull",true);
+	}
+}
