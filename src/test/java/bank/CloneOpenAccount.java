44ed0cd7a0a96846c999aa8458e7f77704138535
LENOVO <LENOVO@DESKTOP-1VN6NSF>
2023-11-28 16:14:03 +0530

    commited clone

---
+++ src/test/java/bank/CloneOpenAccount.java
@@ -0,0 +1,12 @@
+package bank;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CloneOpenAccount {
+	@Test
+	public void opennew()
+	{
+		Reporter.log("Clone Created successfully", true);
+	}
+}
